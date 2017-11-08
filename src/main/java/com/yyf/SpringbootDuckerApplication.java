6c3005d62bfdbdd77add32f91af534d6a6946067
yuanyefu <18329025381@163.com>
2017-11-08 15:28:18 +0800

    dsaa

---
+++ src/main/java/com/yyf/SpringbootDuckerApplication.java
@@ -0,0 +1,12 @@
+package com.yyf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDuckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDuckerApplication.class, args);
+	}
+}
